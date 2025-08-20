94458db77f28c2c9e281c9f1f4cc8d4d647d0066
Your Name <you@example.com>
2025-08-20 12:42:11 +0300

    The error of use memo

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { ArrowRight, ArrowLeft } from "lucide-react";
 import "../styles/HeroSlider.css";
 
